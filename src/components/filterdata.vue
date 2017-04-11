a198b6ccb31744a02c6ad4c1eb2690f372f18e9e
platonovAlexey <kalyawe4ka@gmail.com>
2017-04-11 13:44:56 +0300

    edit filterdata file

--- src/components/filterdata.vue
+++ src/components/filterdata.vue
@@ -1,5 +1,22 @@
 <template>
-		<p> {{ reversedmessage }} </p>
+		<!-- <p> {{ reversedmessage }} </p> -->
+
+		<div>
+			<p class="message__text"> {{ reversedmessage }} </p>
+			<hr>
+			<ul class="demo">
+				<li v-for="value in object">
+					{{ value }}
+				</li>
+			</ul>
+			<hr>
+			<button v-on:click="show = !show">
+				Переключить
+			</button>
+			<transition name="fade">
+				<p v-if="show" class="transition__text">hello</p>
+			</transition>
+		</div>
 </template>
 
 <script>
@@ -8,7 +25,13 @@
 		data () {
 			return{
 				greeting: 'hello',
-				messagenorm: 'как дела'
+				messagenorm: 'хохохо',
+				show: true,
+					object:{
+					firstName: 'Иван',
+					lastName: 'Петров',
+					age: 30
+					}
 			}
 		},
 		computed: {
@@ -27,5 +50,28 @@
 		font-weight: 100; 
 		font-size: 22px;
 		text-align: center;
+	}
+	.message__text{
+		color: yellow;
+	}
+
+	.demo{
+		list-style: none;
+		li{
+			color: green;
+			font-size: 22px;
+		}
+	}
+
+	.transition__text{
+		text-align: left;
+		color: tomato;
+	}
+
+	.fade-enter-active, .fade-leave-active {
+  	transition: opacity .5s
+	}
+	.fade-enter, .fade-leave-to /* .fade-leave-active для <2.1.8 */ {
+	  opacity: 0
 	}
 </style>
